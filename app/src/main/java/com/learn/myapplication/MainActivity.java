466ab173bc01eedf0f9cdd65e886f4bb9aa83f93
308229387 <308229387@qq.com>
2020-12-16 23:08:58 +0800

    textview\imageview\edit

--- app/src/main/java/com/learn/myapplication/MainActivity.java
+++ app/src/main/java/com/learn/myapplication/MainActivity.java
@@ -1,14 +1,21 @@
 package com.learn.myapplication;
 
+import android.os.Bundle;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        image = findViewById(R.id.learn_imageview);
+        image.setBackgroundResource(R.mipmap.ic_launcher);
     }
+
+
 }
